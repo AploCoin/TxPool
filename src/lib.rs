ec69b8423624bb63b5ef948547de5ed26c24d69e
matvey <matvey_popov2004@mail.ru>
2024-05-06 21:51:33 +0300

    transaction check/add

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,28 @@ struct TxPool {
     time_reference: BTreeMap<u64, HashSet<[u8; 32]>>,
 }
 
-impl TxPool {}
+// получение хешей транзакций 
+fn get_tx_hash(transactions: &HashMap<[u8; 32], Box<dyn Transactionable>>) -> Option<[u8; 32]> {
+    for (tx_hash, _) in transactions.iter() {
+        return Some(*tx_hash);
+    }
+    None
+}
+
+impl TxPool {
+
+    pub fn add_transaction(&mut self, transaction: Box<dyn Transactionable>) -> bool {
+        let mut transactions = HashMap::new();
+        let tx_hash = get_tx_hash(&transactions).unwrap();
+        // Проверяем, если хеш(транзакция) есть
+        if transactions.contains_key(&tx_hash) {
+            return false;
+        }    
+            // Если хэша нет, добавляем транзакцию.
+            transactions.insert(tx_hash, transaction);
+
+            true
+    }
+        
+    }
+
