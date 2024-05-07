aea87ecbdbb7452bd4bd9118c371c30cd5de069d
matvey <matvey_popov2004@mail.ru>
2024-05-07 23:28:19 +0300

    try

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::{BTreeMap, HashMap, HashSet};
 
-use blockchaintree::transaction::Transactionable;
+use blockchaintree::transaction::{self, Transactionable};
 
 #[derive(Default)]
 struct TxPool {
@@ -8,28 +8,17 @@ struct TxPool {
     time_reference: BTreeMap<u64, HashSet<[u8; 32]>>,
 }
 
-// получение хешей транзакций 
-fn get_tx_hash(transactions: &HashMap<[u8; 32], Box<dyn Transactionable>>) -> Option<[u8; 32]> {
-    for (tx_hash, _) in transactions.iter() {
-        return Some(*tx_hash);
-    }
-    None
-}
-
 impl TxPool {
 
     pub fn add_transaction(&mut self, transaction: Box<dyn Transactionable>) -> bool {
-        let mut transactions = HashMap::new();
-        let tx_hash = get_tx_hash(&transactions).unwrap();
-        // Проверяем, если хеш(транзакция) есть
-        if transactions.contains_key(&tx_hash) {
-            return false;
-        }    
-            // Если хэша нет, добавляем транзакцию.
-            transactions.insert(tx_hash, transaction);
-
-            true
-    }
+        let tx_hash = transaction.hash();
+        let time = transaction.get_timestamp(); 
         
+        self.transactions.entry(tx_hash)
+            
+            .or_insert(transaction);
+
+        true
     }
+}
 
